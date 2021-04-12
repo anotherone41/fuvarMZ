32f551b80b6e90119f6f0d736bdf6c829016fd6d
anotherone41 <anotherone41fod@hotmail.com>
2021-04-12 19:52:03 +0200

    kész feladat

--- FuvarMZ/Fuvar.cs
+++ FuvarMZ/Fuvar.cs
@@ -8,5 +8,32 @@ namespace FuvarMZ
 {
     class Fuvar
     {
+        public int Id { get; set; }
+        public string Indulas{ get; set; }
+        public int Idotartam { get; set; }
+
+        public double Tavolsag { get; set; }
+        public double Viteldíj { get; set; }
+        public double Borravalo { get; set; }
+        public string FizetesiMod { get; set; }
+
+        public Fuvar(string sor)
+        {
+            string[] s = sor.Split(';');
+            Id = int.Parse(s[0]);
+            Indulas = s[1];
+            Idotartam = int.Parse(s[2]);
+            Tavolsag = double.Parse(s[3]);
+            Viteldíj = double.Parse(s[4]);
+            Borravalo = double.Parse(s[5]);
+            FizetesiMod = s[6];
+        }
+
+
+        /*private int id;
+        public int getId()
+        {
+            return id;
+        }*/
     }
 }
--- FuvarMZ/Program.cs
+++ FuvarMZ/Program.cs
@@ -12,6 +12,100 @@ namespace FuvarMZ
         static void Main(string[] args)
         {
             string[] sorok = File.ReadAllLines("fuvar.csv");
+            List<Fuvar> fuvarok = new List<Fuvar>();
+
+            foreach(string sor  in sorok.Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+            int N = fuvarok.Count;
+            Console.WriteLine($"3. feladat: {N} fuvar");
+
+            //4
+            int fuvarDb = 0;
+            double bevetel = 0;
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if(fuvar.Id ==6185)
+                {
+                    fuvarDb++;
+                    bevetel += fuvar.Viteldíj;
+                }
+            }
+            Console.WriteLine($"4. feladat {fuvarDb} fuvar alatt: {bevetel}");
+            Console.WriteLine("5. feladat");
+            Dictionary<string, int> fizModDb = new Dictionary<string, int>();
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                string kulcs = fuvar.FizetesiMod;
+                if (fizModDb.ContainsKey(kulcs))
+                {
+                    fizModDb[kulcs]++;
+
+                }
+                else
+                {
+                    fizModDb.Add(kulcs, 1);
+                }
+                foreach  (KeyValuePair<string, int> item in fizModDb)
+                {
+                    fizModDb.Add(kulcs, 1);
+                }
+                
+            }
+            //6. feladat
+            double osszMerfold = 0;
+            foreach (Fuvar fuvar1 in fuvarok)
+            {
+                osszMerfold += fuvar.Tavolsag;
+
+            }
+            Console.WriteLine($"{osszMerfold * 1.6:F2} km");
+
+            //7
+            Console.WriteLine($"7. feladat: leghosszabb fuvar: ");
+            int maxIdotartamInd = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (fuvarok[i].Idotartam > fuvarok[maxIdotartamInd].Idotartam)
+                {
+                    maxIdotartamInd = i;
+
+                }
+
+
+
+
+            }
+            Console.WriteLine($"\tfuvar hossza: {fuvarok[maxIdotartamInd].Idotartam} másodperc");
+            Console.WriteLine($"\ttaxi id: {fuvarok[maxIdotartamInd].Id}");
+            Console.WriteLine($"\tmegtett távolság: {fuvarok[maxIdotartamInd].Tavolsag:F1} km");
+            Console.WriteLine($"\tviteldíj: {fuvarok[maxIdotartamInd].Viteldíj} $");
+
+            //8
+            Console.WriteLine("8. feladat: \"hibak.txt\"");
+            List<string> hibasSorok = new List<string>();
+            hibasSorok.Add(sorok[0]);
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if (fuvar.Idotartam > 0 && fuvar.Viteldíj > 0 && fuvar.Tavolsag == 0)
+                {
+                    string sor = "" ;
+                    sor += fuvar.Id + ";";
+                    sor += fuvar.Indulas + ";";
+                    sor += fuvar.Idotartam + ";";
+                    sor += fuvar.Tavolsag + ";";
+                    sor += fuvar.Viteldíj + ";";
+                    sor += fuvar.Borravalo + ";";
+                    sor += fuvar.FizetesiMod + ";";
+
+                }
+            }
+
+            File.WriteAllText("hibak.txt", "", Encoding.UTF8);
+
+            Console.ReadLine();
+
         }
     }
 }
